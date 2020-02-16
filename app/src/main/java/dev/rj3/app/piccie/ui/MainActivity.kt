91029f5b9967ad9553dcecd3df22fd77ac60fe51
RobertJrIII <RJ3_the_dev@outlook.com>
2020-02-15 19:04:32 -0800

    displays toast when navbar is reselected

--- app/src/main/java/dev/rj3/app/piccie/ui/MainActivity.kt
+++ app/src/main/java/dev/rj3/app/piccie/ui/MainActivity.kt
@@ -2,13 +2,10 @@ package dev.rj3.app.piccie.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
 import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationItemView
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import dev.rj3.app.piccie.R
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.toolbar.*
@@ -24,6 +21,8 @@ class MainActivity : AppCompatActivity() {
         navBar.setupWithNavController(navController)
 
         navBar.setOnNavigationItemReselectedListener {
+            Toast.makeText(this@MainActivity, it.itemId,Toast.LENGTH_SHORT).show()
+
         }
         setSupportActionBar(toolbar)
 
