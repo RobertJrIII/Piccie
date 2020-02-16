5150bf299dab436c299463c481fc88a0a147bf5c
RobertJrIII <RJ3_the_dev@outlook.com>
2020-02-15 18:44:56 -0800

    added itemreselectedlisterner in MainActivity.kt

--- app/src/main/java/dev/rj3/app/piccie/ui/MainActivity.kt
+++ app/src/main/java/dev/rj3/app/piccie/ui/MainActivity.kt
@@ -2,9 +2,13 @@ package dev.rj3.app.piccie.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dev.rj3.app.piccie.R
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.toolbar.*
@@ -19,6 +23,8 @@ class MainActivity : AppCompatActivity() {
         val navController: NavController = findNavController(R.id.nav_host_fragment)
         navBar.setupWithNavController(navController)
 
+        navBar.setOnNavigationItemReselectedListener {
+        }
         setSupportActionBar(toolbar)
 
 
